742d16b92192f1e39cb5fc5ccf71125fba2ee360
kingujebeh <kingujebeh@gmail.com>
2025-08-17 22:51:21 +0000

    Production Port enabled

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(morgan("common"));
 
 app.use(router)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Unknown Server Running", PORT);
