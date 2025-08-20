a38536f1053d94f3d0f188d5a3c5a66f21cfe01e
kingujebeh <kingujebeh@gmail.com>
2025-08-20 04:27:51 +0000

    Now Getting index.html For "/"

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,9 @@ function getSubname(domain) {
 const bucket = new Storage().bucket("supercpanel");
 
 function getFile(projectID, subname, filePath) {
+
+  
+
   // organize by first letter to shard if desired
   const prefix = projectID[0];
   return bucket.file(path.join(prefix, projectID, subname, "dist", filePath));
