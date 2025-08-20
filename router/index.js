e50c5301376fda60581af6b9465782bcdae8a9a8
kingujebeh <kingujebeh@gmail.com>
2025-08-20 04:38:57 +0000

    All routes caught

--- router/index.js
+++ router/index.js
@@ -1,12 +1,10 @@
-
 const express = require("express");
 const router = express.Router();
 
 const controller = require("../controller");
 
-
-router.get("/", controller.home);
-
 router.get("/data", controller.data);
+
+router.all("/{*any}", controller.home);
 
 module.exports = router;
