df06f840aa8fb917ccbe2935dfadebd3ab7e3b5c
kingujebeh <kingujebeh@gmail.com>
2025-08-21 11:09:20 +0000

    Blacklist Removed

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,20 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-const { domains } = require("../data");
-
-let domainslist = [];
-
-for (let [key, list] of Object.entries(domains)) {
-  domainslist.push(...list);
-}
-
 const httpsRedirect = (req, res, next) => {
-  if (!domainslist.includes(req.headers.host)) {
-    console.warn(`Blocked request from host: ${req.headers.host}`);
-    return res.status(403).send("Forbidden");
-  }
-
   if (
     process.env.NODE_ENV === "production" &&
     req.headers["x-forwarded-proto"] &&
