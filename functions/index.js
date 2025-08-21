228ccd98826b7e5b93937e6ab0e10c958c6a74b2
kingujebeh <kingujebeh@gmail.com>
2025-08-21 09:55:11 +0000

    Error Fixed. Path Corrected

--- functions/index.js
+++ functions/index.js
@@ -14,14 +14,14 @@ function getFile(subname, reqPath) {
   let cleaned = decodeURIComponent(reqPath);
 
   // organize by first letter to shard if desired
-  const prefix = projectID[0];
 
   // If path appears to be a file (has extension), use it; else fallback to index.html
   const hasExt = path.extname(cleaned) !== "";
   const filePath = hasExt ? cleaned : path.join("index.html");
 
-  console.log("u", "unknown", "default", "dist", subname, filePath);
-  return bucket.file(path.join(prefix, projectID, subname, "dist", filePath));
+  return bucket.file(
+    path.join("u", "unknown", "default", "dist", subname, filePath)
+  );
 }
 
 module.exports = { getSubname, getFile };
