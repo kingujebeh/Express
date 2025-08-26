1b4f5133bc5bf4d5450d4e20878c91d83a6f5754
augmentchinedu <augmentchinedu@gmail.com>
2025-08-26 04:56:08 +0000

    Server Cleaned Up

--- core/index.js
+++ core/index.js
@@ -1,5 +1,3 @@
-require("../secrets");
-
 const init = async () => {
   console.info("Initializing Express");
 };
--- data/index.js
+++ data/index.js
@@ -5,61 +5,8 @@ const domains = {
   shop: ["osasengineering.shop"],
   store: ["yatsar.store"],
   pro: ["creatyve3d.pro"],
-  me: ["supercpanel.nw.r.appspot.com"],
+  me: ["great-unknown.nw.r.appspot.com"],
 };
-
-const users = [
-  {
-    firstName: "Beauty",
-    lastName: "Yoh",
-    email: "yohbeauty@gmail.com",
-  },
-  {
-    firstName: "Henry",
-    lastName: "Uwajeh",
-    email: "spoiltcooker@gmail.com",
-  },
-  {
-    firstName: "Augment",
-    lastName: "Chinedu",
-    email: "augmentchinedu@gmail.com",
-  },
-  {
-    firstName: "Kingsley",
-    lastName: "Ogbu",
-    email: "thekinggroups1@gmail.com",
-  },
-  {
-    firstName: "Nicholas",
-    lastName: "Uwajeh",
-    email: "uwajehnicholas@gmail.com",
-  },
-  {
-    firstName: "Elizabeth",
-    lastName: "Uwajeh",
-    email: "lizzymadukauwajeh@gmail.com",
-  },
-  {
-    firstName: "Rotimi",
-    lastName: "Bello",
-    email: "rotimiisrael@gmail.com",
-  },
-  {
-    firstName: "Sunshine",
-    lastName: "O",
-    email: "theyatsarbrand@gmail.com",
-  },
-  {
-    firstName: "Desmond",
-    lastName: "Osaretin",
-    email: "desmondosaretin989@gmail.com",
-  },
-  {
-    firstName: "Othuke",
-    lastName: "Efe",
-    email: "efealltymz@gmail.com",
-  },
-];
 
 const unknown = {
   name: "Unknown",
