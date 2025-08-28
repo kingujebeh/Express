78349943c6167b6fb6812a9fc24c32155130dcc4
augmentchinedu <augmentchinedu@gmail.com>
2025-08-28 17:57:40 +0000

    User Data Payload Sent Back from Authorization

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -1,12 +1,30 @@
 const { OAuth2Client } = require("google-auth-library");
 const fn = require("../../functions");
 
-const getUser = () => {};
-
 const auth = async (req, res) => {
-  const token = req.body.token;
+  try {
+    const { token } = req.body;
 
-  fn.signIn(token);
+    // Verify Google ID token
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const user = {
+      id: payload.sub,
+      email: payload.email,
+      name: payload.name,
+      picture: payload.picture,
+    };
+
+    //   fn.signIn(token);
+
+    res.send(user, payload);
+  } catch (err) {
+    res.status(401).send(err);
+  }
 };
 
 module.exports = auth;
