33a7f5ef4858231938a7722277058d1b5b3671f0
augmentchinedu <augmentchinedu@gmail.com>
2025-08-28 18:48:36 +0000

    errors not caught

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -5,30 +5,29 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const auth = async (req, res) => {
   console.log(req.body, process.env.GOOGLE_CLIENT_ID);
-  try {
-    const { token } = req.body;
+  const { token } = req.body;
 
-    // Verify Google ID token
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
+  // Verify Google ID token
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
 
-    console.log(ticket)
-    // const payload = ticket.getPayload();
-    // const user = {
-    //   id: payload.sub,
-    //   email: payload.email,
-    //   name: payload.name,
-    //   picture: payload.picture,
-    // };
+  console.log(ticket);
+  // const payload = ticket.getPayload();
+  // const user = {
+  //   id: payload.sub,
+  //   email: payload.email,
+  //   name: payload.name,
+  //   picture: payload.picture,
+  // };
 
-    //   fn.signIn(token);
+  //   fn.signIn(token);
 
-    res.send(ticket);
-  } catch (err) {
-    res.status(401).send(err);
-  }
+  res.send(ticket);
+  //   } catch (err) {
+  //     res.status(401).send(err);
+  //   }
 };
 
 module.exports = auth;
