ce1213ef31dbb1c71721e437dd59a9e325d147e2
augmentchinedu <augmentchinedu@gmail.com>
2025-08-28 18:24:09 +0000

    OAuth2Client client Made

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -1,6 +1,8 @@
 const { OAuth2Client } = require("google-auth-library");
 const fn = require("../../functions");
 
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
 const auth = async (req, res) => {
   try {
     const { token } = req.body;
