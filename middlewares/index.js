fd60a6b66512c1dd16bc66aa46c8229ca74a9e43
genesiskrane <genesiskrane@gmail.com>
2025-08-29 07:25:43 +0000

    JWT Package Added To Middleware to fix bug

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const { getSecret } = require("../secrets");
 
