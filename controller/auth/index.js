bb201139428700c16531f16baba9c62a33eafac3
augmentchinedu <augmentchinedu@gmail.com>
2025-09-19 16:59:12 +0000

    Payload Logged

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -24,6 +24,7 @@ const auth = async (req, res, next) => {
     res.status(401).send(err);
   }
 
+  console.log(payload)
   // Check if user exists in your DB
   // user = await fn.getUser(payload.sub);
 
