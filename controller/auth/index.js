1133b96407be0af280896c6dfc159708d0e42ce6
augmentchinedu <augmentchinedu@gmail.com>
2025-09-21 10:48:28 +0000

    Auth Functions Modified

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -1,50 +1,19 @@
-const axios = require("axios");
-const jwt = require("jsonwebtoken");
-
-const { getSecret } = require("../../secrets");
-
 const fn = require("../../functions");
 
 const auth = async (req, res, next) => {
   const { code } = req.body;
 
   try {
-    // Exchange code for tokens
-    const tokenRes = await axios.post("https://oauth2.googleapis.com/token", {
-      code,
-      client_id: getSecret("GOOGLE_CLIENT_ID"),
-      client_secret: getSecret("GOOGLE_CLIENT_SECRET"),
-      redirect_uri: "postmessage", // required for initCodeClient
-      grant_type: "authorization_code",
-    });
-
-    const { id_token } = tokenRes.data;
-
-    // Decode ID token (JWT) to get Google user info
-    const googleUser = JSON.parse(
-      Buffer.from(id_token.split(".")[1], "base64").toString()
-    );
-
-    // Create your app JWT
-    const jwtToken = jwt.sign(
-      {
-        sub: googleUser.sub,
-        email: googleUser.email,
-        name: googleUser.name,
-        picture: googleUser.picture,
-      },
-      "JWT_SECRET",
-      { expiresIn: "7d" }
-    );
+    const { jwtToken, user } = await fn.signin(code);
 
     res
       .cookie("session", jwtToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // only over HTTPS
         sameSite: "lax", // or "strict"
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       })
-      .send(googleUser);
+      .send(user);
   } catch (err) {
     console.error(err.response?.data || err.message);
     res.status(401).json({ error: "Authentication failed" });
--- functions/auth/index.js
+++ functions/auth/index.js
@@ -1,7 +1,9 @@
+const axios = require("axios");
 const jwt = require("jsonwebtoken");
 
-const { getSecret } = require("../../secrets");
 const db = require("../../db");
+
+const { getSecret } = require("../../secrets");
 
 const signup = async () => {
   user = await db.users.insert({
@@ -16,16 +18,42 @@ const signup = async () => {
 
 const getUser = async (sub) => await db.users.findOne({ googleId: sub });
 
-const getnerateJWT = (payload) =>
-  jwt.sign(
-    {
-      name: payload.name,
-      email: payload.email,
-      picture: payload.picture,
-    },
-    getSecret("JWT_SECRET"),
-    { expiresIn: "7d" } // token expires in 7 days
-  );
-
 const verifyJWT = (token) => jwt.verify(token, getSecret("JWT_SECRET"));
-module.exports = { getUser, signup, getnerateJWT, verifyJWT };
+
+const signin = async () => {
+  try {
+    // Exchange code for tokens
+    const tokenRes = await axios.post("https://oauth2.googleapis.com/token", {
+      code,
+      client_id: getSecret("GOOGLE_CLIENT_ID"),
+      client_secret: getSecret("GOOGLE_CLIENT_SECRET"),
+      redirect_uri: "postmessage", // required for initCodeClient
+      grant_type: "authorization_code",
+    });
+
+    const { id_token } = tokenRes.data;
+
+    // Decode ID token (JWT) to get Google user info
+    const googleUser = JSON.parse(
+      Buffer.from(id_token.split(".")[1], "base64").toString()
+    );
+
+    const user = {
+      uid: googleUser.sub,
+      email: googleUser.email,
+      name: googleUser.name,
+      picture: googleUser.picture,
+    };
+
+    // Create your app JWT
+    const jwtToken = jwt.sign(user, getSecret("JWT_SECRET"), {
+      expiresIn: "30d",
+    });
+
+    return { jwtToken, user };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { getUser, signin, signup, verifyJWT };
