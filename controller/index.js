70753a9a6ae363482f086b1c64d69fd5fc8b3e69
augmentchinedu <augmentchinedu@gmail.com>
2025-09-27 08:54:54 +0000

    Debug made

--- controller/index.js
+++ controller/index.js
@@ -9,47 +9,48 @@ const auth = require("./auth");
 
 const home = async (req, res) => {
   try {
-    let subname =
-      req.subdomains.find((s) => subdomains.includes(s)) ||
-      fn.getSubname(req.headers.host);
+    res.send('Hello World')
+    // let subname =
+    //   req.subdomains.find((s) => subdomains.includes(s)) ||
+    //   fn.getSubname(req.headers.host);
 
-    // fallback to krane if no valid subdomain
-    if (!subname) {
-      subname = "krane";
-    }
+    // // fallback to krane if no valid subdomain
+    // if (!subname) {
+    //   subname = "krane";
+    // }
 
-    const reqPath = req.path || "/";
-    const hasExt = path.extname(reqPath) !== "";
+    // const reqPath = req.path || "/";
+    // const hasExt = path.extname(reqPath) !== "";
 
-    let file;
+    // let file;
 
-    if (hasExt) {
-      // static asset → try subdomain, else fallback to krane
-      const filePath = reqPath.substring(1);
-      file = fn.getFile(subname, filePath);
+    // if (hasExt) {
+    //   // static asset → try subdomain, else fallback to krane
+    //   // const filePath = reqPath.substring(1);
+    //   // file = fn.getFile(subname, filePath);
 
-      let [exists] = await file.exists();
+    //   // let [exists] = await file.exists();
 
-      if (!exists && subname !== "krane") {
-        // fallback to krane assets if subdomain doesn't have the file
-        file = fn.getFile("krane", filePath);
-        [exists] = await file.exists();
-      }
+    //   // if (!exists && subname !== "krane") {
+    //   //   // fallback to krane assets if subdomain doesn't have the file
+    //   //   file = fn.getFile("krane", filePath);
+    //   //   [exists] = await file.exists();
+    //   // }
 
-      if (!exists) {
-        return res.status(404).send("Not found");
-      }
-    } else {
-      // SPA route → always serve krane/index.html
-      file = fn.getFile("krane", "index.html");
-      const [exists] = await file.exists();
-      if (!exists) {
-        return res.status(404).send("App not found");
-      }
-    }
+    //   // if (!exists) {
+    //   //   return res.status(404).send("Not found");
+    //   }
+    // } else {
+    //   // SPA route → always serve krane/index.html
+    //   // file = fn.getFile("krane", "index.html");
+    //   // const [exists] = await file.exists();
+    //   // if (!exists) {
+    //   //   return res.status(404).send("App not found");
+    //   // }
+    // }
 
-    const [buffer] = await file.download();
-    const contentType = mime.lookup(file.name) || "application/octet-stream";
+    // const [buffer] = await file.download();
+    // const contentType = mime.lookup(file.name) || "application/octet-stream";
 
     console.log(file.name);
 
