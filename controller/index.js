b0a15057f606af2fefb44627199b1c7dc2d6fe08
augmentchinedu <augmentchinedu@gmail.com>
2025-09-28 15:15:31 +0000

    console Logged

--- controller/index.js
+++ controller/index.js
@@ -11,6 +11,7 @@ const auth = require("./auth");
 const home = async (req, res, next) => {
   async function sendFile(subname, filePath) {
     try {
+      console.log(subname, filePath);
       const file = await fn.getFile(subname, filePath);
       const contentType = mime.lookup(file.name) || "application/octet-stream";
       const [buffer] = await file.download();
@@ -25,6 +26,7 @@ const home = async (req, res, next) => {
 
       return res.type(contentType).send(buffer);
     } catch (err) {
+      console.log(err)
       return null; // let caller decide fallback
     }
   }
