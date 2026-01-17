6030e922d14d460f01849b49ef52108e4ea7bc66
augmentchinedu <augmentchinedu@gmail.com>
2026-01-17 16:16:43 +0000

    GraphQL Query Fields Updated

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,7 +1,59 @@
 import gql from "graphql-tag";
 
 export const typeDefs = gql`
+  type Product {
+    book: String
+    drink: String
+    hair: String
+    perfume: String
+    vehicle: String
+    watch: String
+  }
+
+  type Institution {
+    academy: String
+    bank: String
+    hospital: String
+    hotel: String
+    school: String
+    university: String
+  }
+
+  type Competition {
+    league: String
+    tournament: String
+    game: String
+  }
+
+  type Events {
+    tournament: String
+    game: String
+    marriages: String
+    seminar: String
+  }
+
   type Query {
     health: String
+    events: Events
+    apps: String
+    games: String
+    courses: String
+    competition: Competition
+    accomodation: String
+    music: String
+    posts: String
+    teams: String
+    users: String
+    vendors: String
+    videos: String
+    institutions: Institution
+    countries: String
+    states: String
+    kingdoms: String
+    realms: String
+    towns: String
+    cities: String
+    products: Product
+    workstations: String
   }
 `;
